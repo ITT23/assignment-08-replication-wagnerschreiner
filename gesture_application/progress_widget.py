e437b4c70ad6720c825d47cfa5c4d9c2d54f419d
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-07-16 15:31:58 +0200

    Add progress for augmentation

--- gesture_application/progress_widget.py
+++ gesture_application/progress_widget.py
@@ -1,24 +1,36 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import config
+from PyQt5 import QtCore, QtWidgets
 
 
 class ProgressWidget(QtWidgets.QWidget):
     def __init__(self,) -> None:
         super().__init__()
         self.setup_UI()
+        self.augmentation = True
 
     def setup_UI(self):
         layout = QtWidgets.QVBoxLayout()
 
-        self.progress_label = QtWidgets.QLabel('Training Model')
+        self.progress_label = QtWidgets.QLabel()
         self.progress_label.setAlignment(QtCore.Qt.AlignCenter)
         self.progress_bar = QtWidgets.QProgressBar()
-        self.progress_bar.setRange(0, 30)
         self.progress_bar.setAlignment(QtCore.Qt.AlignCenter)
 
         layout.addWidget(self.progress_label)
         layout.addWidget(self.progress_bar)
+        layout.addStretch(1)
 
         self.setLayout(layout)
 
-    def update_progress(self, n):
+    def update_training_progress(self, n):
+        self.progress_label.setText(f'Epoch {n} of {config.MAX_EPOCHS}')
         self.progress_bar.setValue(n)
+        self.update()
+
+    def update_augmentation_progress(self, n):
+        self.progress_bar.setValue(n)
+        self.update()
+
+    def init_progress_bar(self, upper_bound):
+        self.progress_label.setText('Generating training set...')
+        self.progress_bar.setRange(0, upper_bound)
